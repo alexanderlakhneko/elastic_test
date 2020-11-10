1057b9b5ccc9e4f28e193195d6d022cde2eadbb6
Alexander Lakhneko <alexanderlakhneko@gmail.com>
2020-11-10 21:06:18 +0200

    Add auth

---
+++ application/resources/js/Jetstream/ActionMessage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <transition leave-active-class="transition ease-in duration-1000" leave-class="opacity-100"
+                    leave-to-class="opacity-0">
+            <div class="text-sm text-gray-600" v-show="on">
+                <slot/>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['on'],
+    }
+</script>
---
+++ application/resources/js/Jetstream/Input.vue
@@ -0,0 +1,17 @@
+<template>
+    <input :value="value" @input="$emit('input', $event.target.value)" class="form-input rounded-md shadow-sm"
+           ref="input">
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+
+        methods: {
+            focus() {
+                this.$refs.input.focus()
+            }
+        }
+    }
+</script>
+
